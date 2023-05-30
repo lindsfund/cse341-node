c80944ced97c4f0304d0bdec1ceb2aa19c3c9815
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-05-30 14:10:32 -0400

    add server.js code

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 //main app file
+
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+app.get('/', (req,res) => {
+    res.send('Hello!');
+});
+
+
+
+router.get('/home', (req,res)=> {
+    res.send('This is the Home Page router');
+});
+
+app.use('/', router);
+
+app.listen(process.env.port || 4000);
+console.log(`Web Server is listening at port ${process.env.port || 4000}`);
