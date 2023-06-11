4b6ed34111a4ec3a58ac7a3d61dd5b96f8b37688
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-06-11 00:40:31 -0600

    rollback4

--- personalAssignments/server.js
+++ personalAssignments/server.js
@@ -1,18 +1,18 @@
-//main app file
-const express = require('express');
-const bodyParse = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const mongodb = require('./db/connect');
-const routes = require('./routes'); //use this for the personal assignments
+// //main app file
+// const express = require('express');
+// const bodyParse = require('body-parser');
+// const MongoClient = require('mongodb').MongoClient;
+// const mongodb = require('./db/connect');
+// const routes = require('./routes'); //use this for the personal assignments
 
-const app = express();
-const port = process.env.port || 4000;
+// const app = express();
+// const port = process.env.port || 4000;
 
 
-//personal assignments
-app.use('/', require('./routes/index'));
+// //personal assignments
+// app.use('/', require('./routes/index'));
 
-app.listen(process.env.port || port);
-console.log(`Web Server is listening at port ${process.env.port || port}`);
+// app.listen(process.env.port || port);
+// console.log(`Web Server is listening at port ${process.env.port || port}`);
 
 
