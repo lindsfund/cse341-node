c7ed1b2fd46b52bc3ea7f295bf175b6e33cc2838
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-06-11 00:23:11 -0600

    rollback save

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const express = require('express');
 const bodyParse = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
+const routes = require('./personalAssignments/routes');
 const professionalRoutes = require('./routes/professional');
 
 const app = express();
+const app2 = express();
 const port = process.env.port || 8080;
+const port2 = process.env.port ||
 
 
-
+//week 1
+app2.use('/', require('./personalAssignments/routes'));
+app2.listen(process.env.port2 || port2);
+console.log(`Web server is listening on ${port2}`);
 //week 2
 app
     .use(bodyParse.json())
