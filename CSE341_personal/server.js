bcaaf0a4f09e2b2f8b2cf6c901229069fd6d7eef
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-06-12 10:11:25 -0600

    init new servers

--- CSE341_personal/server.js
+++ CSE341_personal/server.js
@@ -1,2 +1,12 @@
+const express = require ('express');
+const app = express;
+const port = 4000;
 
+app.get ('/', (req,res) => {
+    res.send('Hello');
+});
+
+app.listen (process.env.PORT || port, () => {
+    console.log(`Web Server is listening at port ${port}`);
+})
  
