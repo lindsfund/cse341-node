e8a7909b459911b6fd7fc49125d4b506d0918bff
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-06-12 11:28:00 -0600

    rollback

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
-const router = require('/routes/.lesson1');
+const router = express.Router();
+const routes = require('/routes/lesson1');
 
 const port = 4000;
 
 app.get ('/', (req, res) => {
     res.send('Hello');
 });
+
 
 app.listen(process.env.PORT || port, () => {
     console.log(`Web Server is listening on port ${port}`);
