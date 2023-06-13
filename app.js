e42d57856da520e223414be6efe261a881c777e6
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-06-13 14:13:54 -0600

    lesson one up to controllers

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const bodyParser = require ('body-parser');
 const app = express();
-// const router = express.Router();
-// const routes = require('/routes/lesson1');
+const router = express.Router();
+const lesson1controller = require('./controllers/lesson 1');
+//*tutorial link codeforgeek.com/express-nodejs-tutorialnpm
+
 
 const port = 4000;
 
-app.get ('/', (req, res) => {
-    res.send('Hello');
-});
+
+//third party middleware - must come before routes
+app.use(bodyParser.json());
+app.use('/',router);
+
+
+
+app.get ('/', lesson1controller.testRoute);
+app.get ('/lindsey', lesson1controller.lindseyRoute);
+app.get ('/benjamin', lesson1controller.benjaminRoute);
 
 
 app.listen(process.env.PORT || port, () => {
---
+++ controllers/lesson 1.js
@@ -0,0 +1,21 @@
+//This is where logic takes place
+
+
+
+const testRoute = (req, res) => {
+    res.send('test');
+}
+
+const lindseyRoute = (req, res) => {
+    res.send('Lindsey Fund');
+}
+
+const benjaminRoute = (req, res) => {
+    res.send('Benjamin Fund');
+}
+
+module.exports = {
+    testRoute,
+    lindseyRoute,
+    benjaminRoute,
+}
