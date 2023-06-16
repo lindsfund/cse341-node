e2a6b9246dd9a2b0f6a20eb1b6ebb512d54bb036
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-06-15 19:48:43 -0600

    used thunder to input body data

--- models/contacts.js
+++ models/contacts.js
@@ -2,15 +2,23 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
+// const contact = {
+//     firstName : req.body.firstName,
+//     lastName : req.body.lastName,
+//     email : req.body.email,
+//     favColor : req.body.favColor,
+//     birthday : req.body.birthday
+// }
 
-const getAll = async (req,res,next) => {
+
+const getAll = async (req,res) => {
     const result = await mongodb.getDb().db().collection('contacts').find();
     result.toArray().then((lists) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(lists);
     });
 };
-const getSingle = async (req, res, next) => {
+const getSingle = async (req, res) => {
     const userId = new ObjectId(req.params.id);
     const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
     result.toArray().then((lists) => {
@@ -19,21 +27,43 @@ const getSingle = async (req, res, next) => {
     });
 };
 
-const addNewContact = async (req, res, next) => {
-    const contact = {firstName: req.body.firstName};
+const addNewContact = async (req, res) => {
+    const contact = {
+        firstName : req.body.firstName,
+        lastName : req.body.lastName,
+        email : req.body.email,
+        favColor : req.body.favColor,
+        birthday : req.body.birthday
+    };
     const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
     if(response.acknowledged){
         res.status(201).json(response);
     } else {
         res.status(500).json(response.error || 'An error occured while adding your contact.')
     }
-   
-    
+}
 
+const updateContact = async (req, res) => {
+    const userId = new ObjectId(req.params.id)
+    const contact = {
+        firstName : req.body.firstName,
+        lastName : req.body.lastName,
+        email : req.body.email,
+        favColor : req.body.favColor,
+        birthday : req.body.birthday
+    };
+    const response = await mongodb.getDb().db().collection('contacts').replaceOne({_id: userId}, contact);
+    console.log(response);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'An error occured while updating your contact.')
+    }
 }
 
 module.exports = {
     getAll,
     getSingle,
     addNewContact,
+    updateContact,
 }
