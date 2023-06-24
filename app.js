081f97ddaa34026ea95131b641bb46f0611e0eaf
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-06-24 14:55:01 -0400

    update for react frontend

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({extended: true}));
 app.use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-key'
+    );
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 })
 app.use('/',require('./routesCtrl/index'));
